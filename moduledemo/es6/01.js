201a647f0337e084e5af8b9220dc97e560629538
hlHang <2710393062@qq.com>
2021-01-26 17:42:27 +0800

    es6写模块化

---
+++ moduledemo/es6/01.js
@@ -0,0 +1,8 @@
+// 定义写法
+export function getList() {
+    console.log('getList')
+}
+
+export function save() {
+    console.log('save.....')
+}
---
+++ moduledemo/es6/02.js
@@ -0,0 +1,5 @@
+// 定义哪些方法可以被其他为你文件调用
+import {getList,save} from "./01.js";
+
+getList()
+save()
