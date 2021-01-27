25757065f8c84ac6ea4c1ae4fa04b39fe9301545
hlHang <2710393062@qq.com>
2021-01-27 11:54:40 +0800

    es6写模块化的第二种写法

---
+++ moduledemo/es6(2)/01.js
@@ -0,0 +1,9 @@
+// 定义写法
+export default {
+    getList() {
+        console.log('getList')
+    },
+    update() {
+        console.log('update...')
+    }
+}
---
+++ moduledemo/es6(2)/02.js
@@ -0,0 +1,5 @@
+// 定义哪些方法可以被其他为你文件调用
+import m from './01.js'
+
+m.getList()
+m.update()
